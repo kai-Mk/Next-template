44827cbfff2a09bb0f7fe3ead6359902fc90b66c
kaitomizuuchi <kaitoshuinei42@gmail.com>
2024-11-10 17:54:35 +0900

    Next.jsセットアップ

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Example from "@/components/Example";
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home</h1>
+            <Example />
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/Example.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Example = () => {
+    return <div>サンプル用コンポーネント</div>;
+};
+
+export default Example;
