d63fe9cf03a2a376fe1cc38d24e299c2c99b98bf
kaitomizuuchi <kaitoshuinei42@gmail.com>
2024-12-09 20:34:50 +0900

    add:ルーティング

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,18 @@
+import Link from 'next/link';
 import React from 'react';
 
 const layout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <html>
-      <body>{children}</body>
-    </html>
-  );
+	return (
+		<html>
+			<body>
+				<nav style={{ gap: '10px', display: 'flex' }}>
+					<Link href="/">Home</Link>
+					<Link href="/about">About</Link>
+				</nav>
+				{children}
+			</body>
+		</html>
+	);
 };
 
 export default layout;
